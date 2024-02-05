c933b9786d3b38dab64dec985f2f4800ee0b7bfa
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-02-05 20:17:32 +0100

    Add badge_generator.py

---
+++ badge_generator.py
@@ -0,0 +1,289 @@
+tools = [
+    {
+        "color": "F7DF1E",
+        "badgeName": "JavaScript",
+        "logoName": "JavaScript",
+        "logoColor": "black",
+    },
+    {
+        "color": "339933",
+        "badgeName": "Node.js",
+        "logoName": "Node.js",
+        "logoColor": "white",
+    },
+    {
+        "color": "61DAFB",
+        "badgeName": "React",
+        "logoName": "React",
+        "logoColor": "black",
+    },
+    {
+        "color": "E34F26",
+        "badgeName": "HTML5",
+        "logoName": "HTML5",
+        "logoColor": "white",
+    },
+    {
+        "color": "007396",
+        "badgeName": "Java",
+        "logoName": "Java",
+        "logoColor": "white"
+    },
+    {
+        "color": "4479A1",
+        "badgeName": "MySQL",
+        "logoName": "MySQL",
+        "logoColor": "white",
+    },
+    {
+        "color": "4169E1",
+        "badgeName": "PostgreSQL",
+        "logoName": "PostgreSQL",
+        "logoColor": "white",
+    },
+    {
+        "color": "3178C6",
+        "badgeName": "TypeScript",
+        "logoName": "TypeScript",
+        "logoColor": "white",
+    },
+    {
+        "color": "47A248",
+        "badgeName": "MongoDB",
+        "logoName": "MongoDB",
+        "logoColor": "white",
+    },
+    {
+        "color": "1572B6",
+        "badgeName": "CSS3",
+        "logoName": "CSS3",
+        "logoColor": "white"
+    },
+    {
+        "color": "239120",
+        "badgeName": "C#",
+        "logoName": "C#",
+        "logoColor": "white"
+    },
+    {
+        "color": "4FC08D",
+        "badgeName": "Vue.js",
+        "logoName": "Vue.js",
+        "logoColor": "white",
+    },
+    {
+        "color": "4EAA25",
+        "badgeName": "Shell",
+        "logoName": "Shell",
+        "logoColor": "white",
+    },
+    {
+        "color": "CC6699",
+        "badgeName": "Sass",
+        "logoName": "Sass",
+        "logoColor": "white"
+    },
+    {
+        "color": "61DAFB",
+        "badgeName": "React Native",
+        "logoName": "React Native",
+        "logoColor": "black",
+    },
+    {
+        "color": "232F3E",
+        "badgeName": "AWS",
+        "logoName": "AWS",
+        "logoColor": "white"
+    },
+    {
+        "color": "4285F4",
+        "badgeName": "Google Cloud",
+        "logoName": "Google Cloud",
+        "logoColor": "white",
+    },
+    {
+        "color": "000000",
+        "badgeName": "Markdown",
+        "logoName": "Markdown",
+        "logoColor": "white",
+    },
+    {
+        "color": "FA7343",
+        "badgeName": "Swift",
+        "logoName": "Swift",
+        "logoColor": "white",
+    },
+    {
+        "color": "0095D5",
+        "badgeName": "Kotlin",
+        "logoName": "Kotlin",
+        "logoColor": "white",
+    },
+    {
+        "color": "003545",
+        "badgeName": "MariaDB",
+        "logoName": "MariaDB",
+        "logoColor": "white",
+    },
+    {
+        "color": "47848F",
+        "badgeName": "Electron",
+        "logoName": "Electron",
+        "logoColor": "white",
+    },
+    {
+        "color": "000000",
+        "badgeName": "Next.js",
+        "logoName": "Next.js",
+        "logoColor": "white",
+    },
+    {
+        "color": "E0234E",
+        "badgeName": "Nest.js",
+        "logoName": "Nest.js",
+        "logoColor": "white",
+    },
+    {
+        "color": "000000",
+        "badgeName": "macOS",
+        "logoName": "macOS",
+        "logoColor": "white",
+    },
+    {
+        "color": "FF6363",
+        "badgeName": "Figma",
+        "logoName": "Figma",
+        "logoColor": "white",
+    },
+    {
+        "color": "FF6C37",
+        "badgeName": "Postman",
+        "logoName": "Postman",
+        "logoColor": "white",
+    },
+    {
+        "color": "F58025",
+        "badgeName": "Stack Overflow",
+        "logoName": "Stack Overflow",
+        "logoColor": "white",
+    },
+    {
+        "color": "00457C",
+        "badgeName": "PayPal",
+        "logoName": "PayPal",
+        "logoColor": "white",
+    },
+    {
+        "color": "F05032",
+        "badgeName": "Git",
+        "logoName": "Git",
+        "logoColor": "white"
+    },
+    {
+        "color": "181717",
+        "badgeName": "GitHub",
+        "logoName": "GitHub",
+        "logoColor": "white",
+    },
+    {
+        "color": "2496ED",
+        "badgeName": "Docker",
+        "logoName": "Docker",
+        "logoColor": "white",
+    },
+    {
+        "color": "007ACC",
+        "badgeName": "VS Code",
+        "logoName": "VS Code",
+        "logoColor": "white",
+    },
+    {
+        "color": "CB3837",
+        "badgeName": "npm",
+        "logoName": "npm",
+        "logoColor": "white"
+    },
+    {
+        "color": "326CE5",
+        "badgeName": "Kubernetes",
+        "logoName": "Kubernetes",
+        "logoColor": "white",
+    },
+    {
+        "color": "8DD6F9",
+        "badgeName": "Webpack",
+        "logoName": "Webpack",
+        "logoColor": "black",
+    },
+    {
+        "color": "000000",
+        "badgeName": "IntelliJ IDEA",
+        "logoName": "IntelliJ IDEA",
+        "logoColor": "white",
+    },
+    {
+        "color": "2088FF",
+        "badgeName": "GitHub Actions",
+        "logoName": "GitHub Actions",
+        "logoColor": "white",
+    },
+    {
+        "color": "4B32C3",
+        "badgeName": "ESLint",
+        "logoName": "ESLint",
+        "logoColor": "white",
+    },
+    {
+        "color": "02303A",
+        "badgeName": "Gradle",
+        "logoName": "Gradle",
+        "logoColor": "white",
+    },
+    {
+        "color": "EE0000",
+        "badgeName": "Ansible",
+        "logoName": "Ansible",
+        "logoColor": "white",
+    },
+    {
+        "color": "147EFB",
+        "badgeName": "Xcode",
+        "logoName": "Xcode",
+        "logoColor": "white",
+    },
+    {
+        "color": "66595C",
+        "badgeName": "Atom",
+        "logoName": "Atom",
+        "logoColor": "white"
+    },
+    {
+        "color": "C21325",
+        "badgeName": "Jest",
+        "logoName": "Jest",
+        "logoColor": "white"
+    },
+    {
+        "color": "F7B93E",
+        "badgeName": "Prettier",
+        "logoName": "Prettier",
+        "logoColor": "black",
+    },
+    {
+        "color": "FBB040",
+        "badgeName": "Homebrew",
+        "logoName": "Homebrew",
+        "logoColor": "black",
+    },
+    {
+        "color": "4A154B",
+        "badgeName": "Slack",
+        "logoName": "Slack",
+        "logoColor": "white",
+    },
+]
+
+for tool, details in tools.items():
+    badge_url = f"https://img.shields.io/badge/-{tool}-{
+        details['color']}?style=flat-square&logo={details['logo']}&logoColor={details['logoColor']}"
+    print(f'<img alt="{tool}" src="{badge_url}" />')
